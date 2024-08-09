4317b14c8ec1ee5e0d4682d03fcd14920aa6e2e8
izmi <izmiukhti@gmail.com>
2024-08-10 01:41:44 +0700

    commit controller

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class welcomeController extends Controller
+{
+    public function hello()
+    {
+        return ('Hello World');
+    }
+
+    public function greeting()
+    {
+        return view('blog.hello', ['name' => 'Izmi']);
+    }
+}
+?>
